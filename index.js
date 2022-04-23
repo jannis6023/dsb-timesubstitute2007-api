7cfc20cfecc6320a7b437cdf4696fe1d22a39dc8
Jannis Gollwitzer <info@jannisgollwitzer.de>
2022-04-24 00:22:44 +0200

    implement demo mode

--- index.js
+++ index.js
@@ -17,115 +17,119 @@ app.use(cors())
 app.get('/substitutes', ((req, res) => {
     const user = req.query.user;
     const pass = req.query.pass;
-    const dsb = new DSB(user, pass);
-    dsb.fetch()
-        .then(data => {
-            const timetables = DSB.findMethodInData('timetable', data);
-            axios.get(timetables.data[0].url, {
-                responseEncoding: 'latin1'
-            })
-                .then(r => {
-                    const html = r.data;
-                    const $ = cheerio.load(html);
-                    $('script').remove()
-                    $('style').remove()
-                    $('head').remove();
-
-                    // Für jeden Tag
-
-                    const days = [];
-
-                    // iterate over days
-                    $('div').each((index, el) => {
-                        const day = {
-                            substitutes: []
-                        }
-                        const blocks = $(el).find('table');
-
-                        // ========================== Datum START ==========================
-                        day.date = $(el).find('table.KBlock.Kopf > tbody > tr:nth-child(1) > td.Datum.ohneumbruch').html()
-                        // ========================== Datum ENDE ==========================
-
-                        // ========================== Fehlende Räume START ==========================
-                        if($(el).remove('table.VorspannBlock').find("table.VorspannBlock:nth-child(2) > tbody > tr > td:nth-child(2)").html() !== null){
-                            day.missingRooms = $(el).find("table.VorspannBlock:nth-child(2) > tbody > tr > td:nth-child(2)").html().split(', ')
-                        }else{
-                            day.missingRooms = []
-                        }
-                        // ========================== Fehlende Räume ENDE ==========================
-
-
-                        // ========================== Bitte beachten START ==========================
-                        if($(el).find('table.BitteBeachtenBlock > tbody > tr > td:nth-child(2)').html() !== null){
-                            day.bitteBeachten = $(el).find('table.BitteBeachtenBlock > tbody > tr > td:nth-child(2)').html()
-                            if (day.bitteBeachten.split('<br>\n\n<br>\n').length > 0) {
-                                let beachtenTeile = day.bitteBeachten.split('<br>\n\n<br>\n');
-                                beachtenTeile = beachtenTeile.map(t => {
-                                    return t.replace(/\n/g, '')
-                                })
-                                day.bitteBeachten = beachtenTeile
-                            }
-                        }else{
-                            day.bitteBeachten = []
-                        }
-                        // ========================== Bitte beachten ENDE ==========================
-
-                        // ========================== Vertretungen START ==========================
-                        if($(el).find('table.VBlock').html() !== null){
-                            const substituteTable = $(el).find('table.VBlock');
-                            const tableRows = substituteTable.find('tr');
-
-                            let id = 1
-                            tableRows.each((rowIndex, rowEl) => {
-                                const row = {
-                                    id: id,
-                                    course: '',
-                                    lesson: '',
-                                    teacher: '',
-                                    substitute: '',
-                                    subject: '',
-                                    room: '',
-                                    description: ''
-                                }
-                                if(rowIndex > 0){
-                                    $(rowEl).find('td').each((colIndex, colEl) => {
-                                        switch (colIndex){
-                                            case 0:
-                                                row.course = $(colEl).text()
-                                                break;
-                                            case 1:
-                                                row.lesson = $(colEl).text()
-                                                break;
-                                            case 2:
-                                                row.teacher = $(colEl).text()
-                                                break;
-                                            case 3:
-                                                row.substitute = $(colEl).text()
-                                                break;
-                                            case 4:
-                                                row.subject = $(colEl).text()
-                                                break;
-                                            case 5:
-                                                row.room = $(colEl).text()
-                                                break;
-                                            case 6:
-                                                row.description = $(colEl).text()
-                                                break;
-                                        }
-                                    })
-                                    day.substitutes.push(row)
-                                    id++;
-                                }
-                            })
-                        }
-                        // ========================== Vertretungen ENDE ==========================
-
-                        days.push(day);
-                    })
-
-                    res.send(days);
+    if(user !== "demo"){
+        const dsb = new DSB(user, pass);
+        dsb.fetch()
+            .then(data => {
+                const timetables = DSB.findMethodInData('timetable', data);
+                axios.get(timetables.data[0].url, {
+                    responseEncoding: 'latin1'
                 })
-        })
+                    .then(r => {
+                        const html = r.data;
+                        const $ = cheerio.load(html);
+                        $('script').remove()
+                        $('style').remove()
+                        $('head').remove();
+
+                        // Für jeden Tag
+
+                        const days = [];
+
+                        // iterate over days
+                        $('div').each((index, el) => {
+                            const day = {
+                                substitutes: []
+                            }
+                            const blocks = $(el).find('table');
+
+                            // ========================== Datum START ==========================
+                            day.date = $(el).find('table.KBlock.Kopf > tbody > tr:nth-child(1) > td.Datum.ohneumbruch').html()
+                            // ========================== Datum ENDE ==========================
+
+                            // ========================== Fehlende Räume START ==========================
+                            if($(el).remove('table.VorspannBlock').find("table.VorspannBlock:nth-child(2) > tbody > tr > td:nth-child(2)").html() !== null){
+                                day.missingRooms = $(el).find("table.VorspannBlock:nth-child(2) > tbody > tr > td:nth-child(2)").html().split(', ')
+                            }else{
+                                day.missingRooms = []
+                            }
+                            // ========================== Fehlende Räume ENDE ==========================
+
+
+                            // ========================== Bitte beachten START ==========================
+                            if($(el).find('table.BitteBeachtenBlock > tbody > tr > td:nth-child(2)').html() !== null){
+                                day.bitteBeachten = $(el).find('table.BitteBeachtenBlock > tbody > tr > td:nth-child(2)').html()
+                                if (day.bitteBeachten.split('<br>\n\n<br>\n').length > 0) {
+                                    let beachtenTeile = day.bitteBeachten.split('<br>\n\n<br>\n');
+                                    beachtenTeile = beachtenTeile.map(t => {
+                                        return t.replace(/\n/g, '')
+                                    })
+                                    day.bitteBeachten = beachtenTeile
+                                }
+                            }else{
+                                day.bitteBeachten = []
+                            }
+                            // ========================== Bitte beachten ENDE ==========================
+
+                            // ========================== Vertretungen START ==========================
+                            if($(el).find('table.VBlock').html() !== null){
+                                const substituteTable = $(el).find('table.VBlock');
+                                const tableRows = substituteTable.find('tr');
+
+                                let id = 1
+                                tableRows.each((rowIndex, rowEl) => {
+                                    const row = {
+                                        id: id,
+                                        course: '',
+                                        lesson: '',
+                                        teacher: '',
+                                        substitute: '',
+                                        subject: '',
+                                        room: '',
+                                        description: ''
+                                    }
+                                    if(rowIndex > 0){
+                                        $(rowEl).find('td').each((colIndex, colEl) => {
+                                            switch (colIndex){
+                                                case 0:
+                                                    row.course = $(colEl).text()
+                                                    break;
+                                                case 1:
+                                                    row.lesson = $(colEl).text()
+                                                    break;
+                                                case 2:
+                                                    row.teacher = $(colEl).text()
+                                                    break;
+                                                case 3:
+                                                    row.substitute = $(colEl).text()
+                                                    break;
+                                                case 4:
+                                                    row.subject = $(colEl).text()
+                                                    break;
+                                                case 5:
+                                                    row.room = $(colEl).text()
+                                                    break;
+                                                case 6:
+                                                    row.description = $(colEl).text()
+                                                    break;
+                                            }
+                                        })
+                                        day.substitutes.push(row)
+                                        id++;
+                                    }
+                                })
+                            }
+                            // ========================== Vertretungen ENDE ==========================
+
+                            days.push(day);
+                        })
+
+                        res.send(days);
+                    })
+            })
+    }else{
+        res.send(JSON.parse(fs.readFileSync('demo.json').toString('latin1')))
+    }
 }))
 app.get('/getDifferences', (req, res) => {
     const user = req.query.user;
